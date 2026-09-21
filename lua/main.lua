be7fc4eea912d0ee88aaae3a28d9c0295e49f1eb
Diablopath69 <belionfx69@gmail.com>
2026-09-22 01:09:43 +0700

    add sae

--- lua/main.lua
+++ lua/main.lua
@@ -84,7 +84,7 @@ local raw_loadstring = loadstring or (getgenv and getgenv().loadstring) or (getf
 
 
 
-local CURRENT_VERSION = "0.1.0"
+local CURRENT_VERSION = "0.2.1"
 local remoteVersionFetched = false
 pcall(function()
     local vSrc = secureFetch("version.txt")
