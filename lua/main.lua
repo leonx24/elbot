497bab962ef65474d8dae6bcb9872ce3358eb043
Diablopath69 <belionfx69@gmail.com>
2026-09-23 02:15:44 +0700

    error kontol 5

--- lua/main.lua
+++ lua/main.lua
@@ -84,7 +84,7 @@ local raw_loadstring = loadstring or (getgenv and getgenv().loadstring) or (getf
 
 
 
-local CURRENT_VERSION = "0.3.3"
+local CURRENT_VERSION = "0.3.4"
 local remoteVersionFetched = false
 pcall(function()
     local vSrc = secureFetch("version.txt")
