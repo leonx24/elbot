166d10d34c135704b9eb57ac2b8c3ddc49f64964
Diablopath69 <belionfx69@gmail.com>
2026-09-26 23:03:49 +0700

    asdkhas UI 6

--- lua/main.lua
+++ lua/main.lua
@@ -84,7 +84,7 @@ local raw_loadstring = loadstring or (getgenv and getgenv().loadstring) or (getf
 
 
 
-local CURRENT_VERSION = "0.5.1"
+local CURRENT_VERSION = "0.5.2"
 local remoteVersionFetched = false
 pcall(function()
     local vSrc = secureFetch("version.txt")
